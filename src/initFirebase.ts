60255cb11be2526d567a34ed5b99167ac69c4b27
Medvedeva Valentina <72570836+medvalna@users.noreply.github.com>
2024-03-27 21:39:01 +0300

    fix: moved init firebase to separate file

---
+++ src/initFirebase.ts
@@ -0,0 +1,23 @@
+﻿import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+const firebaseConfig = {
+  apikey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+};
+
+//const db = firebase.firestore();
+
+function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+}
+
+initFirebase();
+
+export { firebase };
